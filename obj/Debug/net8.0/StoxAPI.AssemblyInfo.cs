48061d836743dd980631be4d92a8b9f3cfb34eda
valon <valonnura888@outlook.com>
2026-01-23 22:25:30 +0100

    logout faqja,settings component, sidebar

--- obj/Debug/net8.0/StoxAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoxAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoxAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+836aef5c13a0f6b7c50eb2e935de3c12c4c0c9fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f06191c1930f05ebd764190134d56aa0843b67c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoxAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoxAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
