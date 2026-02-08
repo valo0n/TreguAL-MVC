16b3671b5ab6509a3b2b82f6eb92b2a831346679
EnsarZeqiri <ensarzeqiri15@gmail.com>
2026-02-08 22:49:28 +0100

    fix: load logout modal script globally and remove duplicate site.js

--- obj/Debug/net8.0/StoxAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoxAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoxAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f65222c907fc56907228db4d7fced2eb5f34100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e03cb98b3f5bdfc815a8844223da13c5f6092dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoxAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoxAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
